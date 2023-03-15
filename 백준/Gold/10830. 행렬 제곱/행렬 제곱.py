3fb7ced5f1398a81f4d9abb1c5c236f4f8f83100
minseok1109 <56787096+minseok1109@users.noreply.github.com>
2023-03-15 13:47:49 +0900

    [Gold IV] Title: 행렬 제곱, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
@@ -1,36 +1,36 @@
 import sys
 
 input = sys.stdin.readline
+
 n, b = map(int, input().strip().split())
 matrix = [list(map(int, input().strip().split())) for _ in range(n)]
 
 
-def matrixSquared(n, matrix1, matrix2):
-    result = [[0 for j in range(n)] for i in range(n)]
+def multi(n, matrix1, matrix2):
+    result = [[0 for _ in range(n)] for _ in range(n)]
 
     for i in range(n):
         for j in range(n):
             for k in range(n):
                 result[i][j] += matrix1[i][k] * matrix2[k][j]
-            result[i][j] %= 1000
+            result[i][j] = result[i][j] % 1000
     return result
 
 
-def powers(n, matrix, b):
+def cal(n, matrix, b):
     if b == 1:
         return matrix
-    elif b == 2:
-        return matrixSquared(n, matrix, matrix)
+
+    # matrix ^ 2
+    tmp = cal(n, matrix, b // 2)
+    if b % 2 == 0:
+        return multi(n, tmp, tmp)
     else:
-        tmp = powers(n, matrix, b // 2)
-        if b % 2 == 0:
-            return matrixSquared(n, tmp, tmp)
-        else:
-            return matrixSquared(n, matrixSquared(n, tmp, tmp), matrix)
+        return multi(n, multi(n, tmp, tmp), matrix)
 
 
-result = powers(n, matrix, b)
+result = cal(n, matrix, b)
 for row in result:
-    for num in row:
-        print(num % 1000, end=" ")
+    for r in row:
+        print(r % 1000, end=" ")
     print()
