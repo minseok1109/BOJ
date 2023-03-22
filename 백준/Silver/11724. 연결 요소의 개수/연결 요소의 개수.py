2270143205603e2b9d8a7c3b212e256585e88239
minseok1109 <56787096+minseok1109@users.noreply.github.com>
2023-03-22 21:25:19 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 668 ms, Memory: 65308 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,26 +1,26 @@
 import sys
 
+sys.setrecursionlimit(10**8)
 input = sys.stdin.readline
-N, M = map(int, input().split())
-visited = [False] * (N + 1)
-graph = [[] for _ in range(N + 1)]
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [False for _ in range(n + 1)]
 
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-count = 0
+for _ in range(m):
+    start, end = map(int, input().split())
+    graph[start].append(end)
+    graph[end].append(start)
 
 
 def dfs(start):
-    visited[start] = True
     for i in graph[start]:
         if not visited[i]:
+            visited[i] = True
             dfs(i)
 
 
-for i in range(1, N + 1):
+count = 0
+for i in range(1, n + 1):
     if not visited[i]:
         dfs(i)
         count += 1
