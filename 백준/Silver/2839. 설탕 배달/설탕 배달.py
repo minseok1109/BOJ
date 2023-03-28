0ef67495cd6e5f81cc5ede7c4470f8a0eff3acaa
minseok1109 <56787096+minseok1109@users.noreply.github.com>
2023-03-28 13:43:14 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+dp = [float("inf")] * (n + 5)
+dp[3] = dp[5] = 1
+
+for i in range(6, n + 1):
+    dp[i] = min(dp[i - 3], dp[i - 5]) + 1
+
+print(dp[n] if dp[n] < float("inf") else -1)
