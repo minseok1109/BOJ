0224fce6d4c13e208dc01665f640dc18d9da38fb
minseok1109 <56787096+minseok1109@users.noreply.github.com>
2023-07-19 15:56:17 +0900

    [level 2] Title: H-Index, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -0,0 +1,11 @@
+function solution(citations) {
+  citations.sort((a, b) => b - a);
+  
+  let index = 0;
+  while (index <= citations.length) {
+    if (index + 1 <= citations[index]) {
+      index++;
+    } else break;
+  }
+  return index;
+}
